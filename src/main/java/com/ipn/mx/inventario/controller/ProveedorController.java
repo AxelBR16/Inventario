6d2ac1ec00d20cf525e69fb42c83271d60a715d4
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2025-01-12 23:23:42 -0600

    Correccion de errores

--- src/main/java/com/ipn/mx/inventario/controller/ProveedorController.java
+++ src/main/java/com/ipn/mx/inventario/controller/ProveedorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ver1/proveedores")
+@RequestMapping("/api/proveedores")
 @CrossOrigin(origins = {"*"})
 
 public class ProveedorController {
